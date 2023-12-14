00baf808a9015ac3b387dc2c299cf9bbf3836b1f
Arthur Robine <arthur.robine@epitech.eu>
2023-12-15 00:33:38 +0100

    Search page layout and enums
    Setup the search page to be ready to use the API.
    Add the enums.dart file.

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flex_market/utils/constants.dart';
+import 'package:flex_market/utils/enums.dart';
 import 'package:flutter/material.dart';
 
 /// Search page with query options
@@ -15,9 +16,25 @@ class SearchPageWidgetState extends State<SearchPageWidget> {
   /// Search query text input value
   String searchText = '';
 
+  /// Search query gender parameter
+  SearchPageGender? selectedGender = SearchPageGender.all;
+
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final List<String> categories = <String>[
+      'Tops',
+      'Bottoms',
+      'Dresses',
+      'Outer Wear',
+      'Under Wear',
+      'Foot Wear',
+      'Accessories',
+      'Sleep Wear',
+      'Athletic',
+      'Swimm Wear',
+    ];
 
     return SingleChildScrollView(
       child: Column(
@@ -72,7 +89,112 @@ class SearchPageWidgetState extends State<SearchPageWidget> {
               ),
             ),
           ),
-          // Add other widgets here that might depend on the searchText
+          Container(
+            width: screenWidth,
+            height: 50,
+            decoration: const BoxDecoration(
+              border: Border(bottom: BorderSide(color: Color(0xFF3D3D3B))),
+            ),
+            child: ToggleButtons(
+              constraints: BoxConstraints.expand(width: screenWidth / 3),
+              isSelected: SearchPageGender.values.map((SearchPageGender g) => g == selectedGender).toList(),
+              onPressed: (int index) {
+                setState(() {
+                  selectedGender = SearchPageGender.values[index];
+                });
+              },
+              borderColor: Colors.grey,
+              selectedBorderColor: Colors.black,
+              fillColor: Colors.transparent,
+              selectedColor: Theme.of(context).colorScheme.secondary,
+              color: Theme.of(context).colorScheme.secondary,
+              borderWidth: 1,
+              borderRadius: BorderRadius.circular(0),
+              renderBorder: false,
+              children: SearchPageGender.values.map((SearchPageGender gender) {
+                final bool isSelected = selectedGender == gender;
+
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 6,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: SearchPageGender.values.indexOf(gender) != 2
+                          ? Border(
+                              right: BorderSide(color: Theme.of(context).colorScheme.secondary),
+                            )
+                          : null,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 29,
+                    ),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: isSelected ? const Color(0xFF3D3D3B) : Colors.transparent,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(4),
+                        ),
+                      ),
+                      child: Center(child: Text(gender.name.toString().toUpperCase())),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          ...categories.map((String category) {
+            return Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                border: const Border(bottom: BorderSide(color: Color(0xFF3D3D3B))),
+              ),
+              child: InkWell(
+                onTap: () {
+                  // Handle category selection
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 29,
+                    top: 13,
+                  ),
+                  child: Text(category),
+                ),
+              ),
+            );
+          }),
+          Container(
+            margin: const EdgeInsets.only(top: margin),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color(0xFF3D3D3B),
+              ),
+              borderRadius: const BorderRadius.all(Radius.circular(12)),
+              color: Colors.transparent,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 3),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle search action
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF3D3D3B),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Search',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/utils/enums.dart
@@ -0,0 +1,23 @@
+/// Enum for the genders
+enum Gender {
+  /// Men
+  men,
+
+  /// Women
+  women,
+
+  /// Both genders
+  unisex,
+}
+
+/// Enum used to provide the search page gender [ToggleButton] values
+enum SearchPageGender {
+  /// Both genders
+  all,
+
+  /// Men
+  men,
+
+  /// WOMEN
+  women,
+}
