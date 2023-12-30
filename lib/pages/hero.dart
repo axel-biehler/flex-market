6b2e23997f076ad2352204718e89406a9a613315
Arthur Robine <arthur.robine@epitech.eu>
2023-12-30 23:49:22 +0100

    fix: lint

--- lib/pages/hero.dart
+++ lib/pages/hero.dart
@@ -1,6 +1,5 @@
 import 'package:flex_market/providers/auth_provider.dart';
 import 'package:flex_market/utils/constants.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
